1ed3e1a01ac1aa7446d318183528af4261963b63
HossamMahmoudKhedr <nhem318@gmail.com>
2023-09-19 14:20:31 +0200

    modify place.py file

--- models/place.py
+++ models/place.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
-
+from models import storage
+from sqlalchemy.orm import relationship
 
 class Place(BaseModel):
     """ A place to stay """
@@ -16,3 +17,15 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+    reviews = relationship("Review", backref='place')
+
+    @property
+    def reviews(self):
+        """getter attribute returns the list of Review instances"""
+        from models.review import Review
+        list = []
+        all_reviews = storage.all(Review)
+        for review in  all_reviews.values():
+            if review.place_id == self.id:
+                list.append(review)
+        return list
