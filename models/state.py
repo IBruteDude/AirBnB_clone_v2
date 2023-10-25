eb51ebf9ed4739d1ab7df09292eafaa09c0025ec
IBruteDude <kamarf505@gmail.com>
2023-10-26 02:37:43 +0300

    Added missing State.cities getter method

--- models/state.py
+++ models/state.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models import storage
 from models.base_model import BaseModel, Base
+from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -9,4 +11,17 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade='all, delete')
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        from models.city import City
+        cities = relationship("City", backref="state", cascade='all, delete')
+    else:
+        @property
+        def cities(self):
+            """an amenities property for handling associated amenities"""
+            from models.city import City
+            city_list = []
+            for city in storage.all(City).values():
+                assert type(city) is City
+                if self.id == city.state_id:
+                    city_list.append(city)
+            return city_list
